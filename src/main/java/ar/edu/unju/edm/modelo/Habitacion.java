0bb98db6efdef21f82cdaf2178306ba4fb7bd03e
admin <admin@LAPTOP-0PALGUM3>
2021-02-16 19:41:42 -0300

    Creacion de Tablas

--- src/main/java/ar/edu/unju/edm/modelo/Habitacion.java
+++ src/main/java/ar/edu/unju/edm/modelo/Habitacion.java
@@ -1,5 +1,122 @@
 package ar.edu.unju.edm.modelo;
 
-public class Habitacion {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.stereotype.Component;
+@Component
+@Entity
+
+public class Habitacion implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID=1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO,generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private long id;
+	@Column
+	private String tipo;
+	@Column
+	private int numhab;
+	@Column
+	private String descripcion;
+	@Column
+	private int precio;
+	public Habitacion() {
+		
+	}
+	public Habitacion(long id, String tipo, int numhab, String descripcion, int precio) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.numhab = numhab;
+		this.descripcion = descripcion;
+		this.precio = precio;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getNumhab() {
+		return numhab;
+	}
+	public void setNumhab(int numhab) {
+		this.numhab = numhab;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + numhab;
+		result = prime * result + precio;
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Habitacion other = (Habitacion) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (id != other.id)
+			return false;
+		if (numhab != other.numhab)
+			return false;
+		if (precio != other.precio)
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Habitacion [id=" + id + ", tipo=" + tipo + ", numhab=" + numhab + ", descripcion=" + descripcion
+				+ ", precio=" + precio + "]";
+	}
+	
+	
 }
--- src/main/java/ar/edu/unju/edm/modelo/Usuario.java
+++ src/main/java/ar/edu/unju/edm/modelo/Usuario.java
@@ -1,5 +1,149 @@
 package ar.edu.unju.edm.modelo;
 
-public class Usuario {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+@Entity
+public class Usuario implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID= 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO,generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long id;
+	@Column
+	String nombreUsuario;
+	@Column
+	String contraseña;
+	@Column
+	int dni;
+	@Column
+	String nombre;
+	@Column
+	String apellido;
+	
+	public Usuario(Long id, String nombreUsuario, String contraseña, int dni, String nombre, String apellido) {
+		super();
+		this.id = id;
+		this.nombreUsuario = nombreUsuario;
+		this.contraseña = contraseña;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombreUsuario=" + nombreUsuario + ", contraseña=" + contraseña + ", dni=" + dni
+				+ ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((contraseña == null) ? 0 : contraseña.hashCode());
+		result = prime * result + dni;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((nombreUsuario == null) ? 0 : nombreUsuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (contraseña == null) {
+			if (other.contraseña != null)
+				return false;
+		} else if (!contraseña.equals(other.contraseña))
+			return false;
+		if (dni != other.dni)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (nombreUsuario == null) {
+			if (other.nombreUsuario != null)
+				return false;
+		} else if (!nombreUsuario.equals(other.nombreUsuario))
+			return false;
+		return true;
+	}
+	
 }
